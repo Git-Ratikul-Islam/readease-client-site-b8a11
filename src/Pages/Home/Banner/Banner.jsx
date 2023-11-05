aa3d271cfb208cc6c1c83aa1b64a591142b5c796
Ratikulislam <joyofficial80@gmail.com>
2023-11-06 00:00:52 +0600

    aos-animation-added-to-banner-text

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -37,7 +37,9 @@ const Banner = () => {
                                           <div className="hero min-h-screen absolute">
                                                 <div className="hero-content flex-col lg:flex-row ">
                                                       <img src="https://i.ibb.co/PTDLLSM/Art-history-mockup1.png" className="test lg:max-w-md max-w-xs rounded-lg shadow-2xl relative lg:bottom-[530px] bottom-72  lg:mr-0 w-1/2" />
-                                                      <div className='relative bottom-[280px] ml-7 lg:right-0 right-20  lg:bottom-[450px] w-1/2'>
+                                                      <div data-aos="fade-down"
+                                                            data-aos-easing="linear"
+                                                            data-aos-duration="1000" className='relative bottom-[280px] ml-7 lg:right-0 right-20  lg:bottom-[450px] w-1/2'>
                                                             <h1 className="lg:text-5xl text-4xl lg:pb-40 pb-4 lg:text-start  lg:ml-20 ml-28 font-bold">Borrow Your Book  Now!</h1>
                                                             <div className='text-start lg:ml-20 pl-28 lg:pl-0'>
                                                                   <p className='text-[#6a6a6b]  text-center lg:text-start '>See all of the available books</p>
