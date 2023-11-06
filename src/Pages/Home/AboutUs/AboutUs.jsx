c074a9b906c3ea2e75095b6e0e157ef1cfe04882
Ratikulislam <joyofficial80@gmail.com>
2023-11-06 10:41:12 +0600

    footer-created-changed-bg-match-web-theme-

---
+++ src/Pages/Home/AboutUs/AboutUs.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+const AboutUs = () => {
+      return (
+            <div className="hero min-h-screen ">
+                  <div className="hero-content flex-col lg:flex-row-reverse w-full flex justify-between">
+                        <img src="https://i.ibb.co/CPkqXyK/The-Lord-Paradise-Mockup2.png" className="max-w-sm w-1/2 rounded-lg shadow-2xl" />
+                        <div className=" w-1/2 text-center ">
+                              <h2 className="lg:text-5xl text-3xl font-semibold text-[#262626s]">About us</h2>
+                              <p className="py-6 items-center lg:text-[18px] text-[16px] text-[#8c8c8c]">"Welcome to ReadEase, your ultimate destination for all things literary! At ReadEase, we're passionate about books and reading, and our goal is to create an enriching and accessible library experience for book enthusiasts of all ages.
+
+                              </p>
+                              <p className="py-6 items-center lg:text-[18px] text-[16px] text-[#8c8c8c]"> Our dedicated team of librarians and book lovers work tirelessly to curate and expand our library to cater to your diverse reading tastes. We're committed to promoting the joy of reading and the power of knowledge, fostering a community of lifelong learners."</p>
+
+                        </div>
+                  </div>
+            </div>
+      );
+};
+
+export default AboutUs;
