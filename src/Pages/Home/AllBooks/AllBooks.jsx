89fc67c5f6131a1e82811dac7615c05f3a3de11b
Ratikulislam <joyofficial80@gmail.com>
2023-11-08 17:01:18 +0600

    created-filter-method-at-all-books-via-available-quantity

--- src/Pages/Home/AllBooks/AllBooks.jsx
+++ src/Pages/Home/AllBooks/AllBooks.jsx
@@ -1,22 +1,44 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import BookCard from "./BookCard";
 
 const AllBooks = () => {
       const books = useLoaderData();
+      const [sortOrder, setSortOrder] = useState("asc"); // Default to ascending order
+
+      const handleFilterClick = () => {
+            // Toggle sorting order between ascending and descending
+            setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+      };
+
       return (
-            <div className="w-5/6 mx-auto text-center min-h-screen">
-                  <h1 className="mt-10 text-4xl font-semibold  w-4/6 mx-auto">Collection of all books</h1>
-                  <hr className="w-4/6 mx-auto mt-5 h-[2px] bg-[#cbe4eb]" />
+            <>
+                  <div className="w-5/6 mx-auto text-center min-h-screen">
+                        <h1 className="mt-10 text-4xl font-semibold w-4/6 mx-auto">
+                              Collection of all books
+                        </h1>
+                        <hr className="w-4/6 mx-auto mt-5 h-[2px] bg-[#cbe4eb]" />
+                        <div className="items-start justify-start flex pt-5">
+                              <button
+                                    className="before:ease relative h-12 w-28 overflow-hidden border-2 border-[#cbe4eb] text-black shadow-2xl transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-[#cbe4eb] before:duration-300 hover:text-black hover:shadow-[#cbe4eb] hover:before:h-64 hover:before:-translate-y-32"
+                                    onClick={handleFilterClick}
+                              >
+                                    <span className="relative z-10">Filter</span>
+                              </button>
+                        </div>
 
-                  <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
-                        {
-                              books.map(book => <BookCard key={book._id}
-                                    book={book}></BookCard>)
-                        }
-
+                        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
+                              {books
+                                    .slice()
+                                    .sort((a, b) =>
+                                          sortOrder === "asc" ? a.quantity - b.quantity : b.quantity - a.quantity
+                                    )
+                                    .map((book) => (
+                                          <BookCard key={book._id} book={book}></BookCard>
+                                    ))}
+                        </div>
                   </div>
-
-            </div>
+            </>
       );
 };
 
--- src/Pages/Home/AllBooks/BookCard.jsx
+++ src/Pages/Home/AllBooks/BookCard.jsx
@@ -3,31 +3,32 @@ import { Link } from "react-router-dom";
 const BookCard = ({ book }) => {
       const { image, name, author, category, rating, _id } = book;
       return (
-            <div className="grid grid-cols-4">
-                  <div className=" w-96 border border-[#abc7cb] mt-14 mb-4">
-                        <figure className="px-10 pt-10">
-                              <img data-aos="zoom-out-down" data-aos-easing="linear"
-                                    data-aos-duration="500" src={image} alt="Shoes" className="" />
-                        </figure>
-                        <div className="card-body items-start text-start ml-3">
-                              <h2 className="card-title font-semibold text-3xl text-[#1f2937bc]">{category}</h2>
-                              <h2 className="card-title font-semibold text-sm text-[#1f2937bc]">{author}</h2>
-                              <h2 className="card-title">{name}</h2>
-                              <p className="">Rating: {rating}</p>
+            <>
 
-                              <div className="card-actions justify-between">
-                                    <Link to={`/updateBook/${_id}`}>
-                                          <button className="before:ease relative h-12 w-28 overflow-hidden border-2 border-[#cbe4eb] text-black shadow-2xl  transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-[#cbe4eb] before:duration-300 hover:text-black hover:shadow-[#cbe4eb] hover:before:h-64 hover:before:-translate-y-32">
-                                                <span className="relative z-10">Update</span>
-                                          </button>
-                                    </Link>
-                                    <button className="before:ease relative h-12 w-28 overflow-hidden border-2 border-[#cbe4eb] text-black shadow-2xl transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-[#cbe4eb] before:duration-300 hover:text-black hover:shadow-[#cbe4eb] hover:before:h-64 hover:before:-translate-y-32">
-                                          <span className="relative z-10">Filter</span>
-                                    </button>
+                  <div className="grid grid-cols-4">
+                        <div className=" w-96 border border-[#abc7cb] mt-10 mb-4">
+                              <figure className="px-10 pt-10">
+                                    <img data-aos="zoom-out-down" data-aos-easing="linear"
+                                          data-aos-duration="500" src={image} alt="Shoes" className="" />
+                              </figure>
+                              <div className="card-body items-start text-start ml-3">
+                                    <h2 className="card-title font-semibold text-3xl text-[#1f2937bc]">{category}</h2>
+                                    <h2 className="card-title font-semibold text-sm text-[#1f2937bc]">{author}</h2>
+                                    <h2 className="card-title">{name}</h2>
+                                    <p className="">Rating: {rating}</p>
+
+                                    <div className="card-actions justify-between">
+                                          <Link to={`/updateBook/${_id}`}>
+                                                <button className="before:ease relative h-12 w-28 overflow-hidden border-2 border-[#cbe4eb] text-black shadow-2xl  transition-all before:absolute before:top-1/2 before:h-0 before:w-64 before:origin-center before:-translate-x-20 before:rotate-45 before:bg-[#cbe4eb] before:duration-300 hover:text-black hover:shadow-[#cbe4eb] hover:before:h-64 hover:before:-translate-y-32">
+                                                      <span className="relative z-10">Update</span>
+                                                </button>
+                                          </Link>
+
+                                    </div>
                               </div>
                         </div>
                   </div>
-            </div>
+            </>
       );
 };
 
